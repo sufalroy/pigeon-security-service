0537a4af26fe36288b05103002256acb2093c90c
sufal <sufalroy1997@gmail.com>
2023-01-28 22:39:53 +0530

    initial spring-boot project setup

---
+++ src/main/java/com/skytel/pigeonsecurityservice/PigeonSecurityServiceApplication.java
@@ -0,0 +1,13 @@
+package com.skytel.pigeonsecurityservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PigeonSecurityServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PigeonSecurityServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skytel/pigeonsecurityservice/PigeonSecurityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skytel.pigeonsecurityservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PigeonSecurityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
